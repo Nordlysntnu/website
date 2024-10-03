e26805625fd3e42a873511673163f446bea99cdc
Emil Vellan <emilvell@stud.ntnu.no>
2024-10-03 19:43:24 +0200

    Made a new folder called data where I put the "members" code. Also added defining comments in the teams page.

--- components/TeamGroup.js
+++ components/TeamGroup.js
@@ -2,14 +2,15 @@ import styles from '../styles/TeamGroup.module.css';
 import Content from './Content';
 import TeamMember from './TeamMember';
 
-export default function TeamGroup({ groupName, members }) {
+export default function TeamGroup({ groupName, members, text }) {
     const {name, position, image} = members
     console.log(members[0].position)
     return (
         <div className={styles.container}>
             <center>
-                <h1 className={styles.title}>{groupName}</h1>
-            </center>
+                <h1><u>{groupName}</u></h1>
+                <h4 className={styles.position}>{text}</h4>
+                </center>
             <div className={styles.memberContainer}>
                 {members.map(function(member) {
                     return (
