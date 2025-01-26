3b64a08048f039b50aa735eb05b187271c2be48b
Emil Vellan <emilvell@stud.ntnu.no>
2025-01-26 15:23:50 +0100

    Quick fix for removing description team site

--- components/TeamOverview.js
+++ components/TeamOverview.js
@@ -12,9 +12,9 @@ const groupDescriptions = {
     'Strategy': <p><b>The Strategy group</b>  is responsible for analyzing data and developing race plans to maximize the car’s performance during competitions. The group uses insight in weather patterns, energy consumption, and road conditions to create plans for the most efficient way to complete a competition. Without good race plans and strategies, even the best of solar cars will be left behind, which makes the strategy group vital for Nordlys’ chase to become world leading.</p>,
     'Brand': <p><b>The Brand group</b> shapes the public image of Nordlys, aiming to build awareness and excitement in our team’s mission. Thru social media, stands, financial connections and web solutions the brand group works towards gaining sponsorships, recruiting talent and improving the overall brand of Nordlys. As in-house talent and sponsorships are vital for our organization’s survival, the brand makes up an important supporting pilar in our organization.</p>,
     'Logistics': <p><b>The Logistics group</b> is responsible for all transportation and logistics related to the competition in a cost-effective manner. This includes everything from transportation planning, customs clearance, documentation, equipment handling, supply chain logistics, and compliance with various regulations that must be followed. The team also ensures that every piece of equipment, from the solar car itself to the smallest tools, arrives on time and in perfect condition. They manage both international and domestic logistics, coordinating with shipping companies, airlines, and local service providers to streamline the process. The group is essential in allowing the rest of the team to focus solely on the competition without having to worry about the practicalities of transport and logistics. Their work guarantees that all resources are in the right place at the right time, enabling the team to perform at their best.</p>,
-    'Technical advisor': <p></p>,
+    //'Technical advisor': <p></p>,
     'Alumni': <p><b>The Alumni</b> consists of former Nordlys members. We are proud to present our former members who all contributed to shaping Nordlys into the organization it is today.</p>,
-    'All active members': <p> </p>
+    //'All active members': <p> </p>
 };
 
 const TeamOverview = () => {
@@ -63,6 +63,8 @@ const TeamOverview = () => {
             <TeamGroup members={groupMembers} />
 
             {/* Information section */}
+
+            {groupDescriptions[selectedGroup] ? 
             <div className={styles.groupInfo}>
                 <div className={styles.description}>
                     {groupDescriptions[selectedGroup]}
@@ -71,6 +73,9 @@ const TeamOverview = () => {
                     <img src={`/${selectedGroup}team.png`} alt={`${selectedGroup}`} />
                 </div>
             </div>
+            :
+            <></>
+            }
         </div>
     );
 };
