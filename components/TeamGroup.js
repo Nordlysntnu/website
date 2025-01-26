cb11ded73741c7e95938f59f883bf482f173baf8
Emil Vellan <emilvell@stud.ntnu.no>
2025-01-26 15:08:49 +0100

    merging Emil and main post-team-site update

--- components/TeamGroup.js
+++ components/TeamGroup.js
@@ -1,24 +1,23 @@
+import React from 'react';
+import TeamMember from './TeamMember'; // Component for individual members
 import styles from '../styles/TeamGroup.module.css';
-import Content from './Content';
-import TeamMember from './TeamMember';
 
-export default function TeamGroup({ groupName, members, text }) {
-    const {name, position, image} = members
-    console.log(members[0].position)
+const TeamGroup = ({ members }) => {
     return (
         <div className={styles.container}>
-            <center>
-                <h1>{groupName}</h1>
-                <p1 className={styles.position}>{text}</p1>
-                </center>
-                <br></br>
             <div className={styles.memberContainer}>
-                {members.map(function(member) {
-                    return (
-                        <TeamMember name={member.name} position={member.position} image={member.image} email={member.email} />
-                    )
-                })}
+                {members.map(member => (
+                    <TeamMember 
+                        key={member.id}
+                        name={member.name}
+                        title={member.title}
+                        image={member.image}
+                        email={member.email}
+                    />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
+
+export default TeamGroup;
