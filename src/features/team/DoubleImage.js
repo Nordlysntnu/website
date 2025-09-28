11550bf406ec482bcbcb4eda3b31d8920945f69d
Maria <145002050+marikolafs@users.noreply.github.com>
2025-09-28 18:57:09 +0200

    refactor: update imports

--- src/features/team/DoubleImage.js
+++ src/features/team/DoubleImage.js
@@ -5,12 +5,12 @@ export default function DoubleImage({ imageFront, imageBack }) {
     return (
         <div className={styles.container}>
             <div className={styles.imageContainer}>
-                <img src={imageFront} className={styles.image} />
-                <img src={imageBack} className={styles.image} />
+
+                <Image src={imageFront} className={styles.image} loading="lazy" placeholder="empty" />
+                <Image src={imageBack} className={styles.image} loading="lazy" placeholder="empty" />
+
             </div>
         </div>
     )
 }
 
-// <Image src={imageFront} width={800} height={500} className={styles.image} loading="lazy" placeholder="empty" />
-// <Image src={imageBack} width={800} height={500} className={styles.image} loading="lazy" placeholder="empty" />
