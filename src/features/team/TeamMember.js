c73c429fdc0e80d3023b04d59bb28f99c1c0d624
Maria <145002050+marikolafs@users.noreply.github.com>
2025-09-28 18:57:09 +0200

    refactor: update imports

--- src/features/team/TeamMember.js
+++ src/features/team/TeamMember.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image'
 import styles from './styles/TeamMember.module.css';
 
 const TeamMember = ({ name, title, image, email }) => {
@@ -17,7 +18,6 @@ const TeamMember = ({ name, title, image, email }) => {
     );}
     else return (
         <div className={styles.container}>
-            // width and height can be updated to better suit the website
             <Image className={styles.image} src={image} alt={`${name}'s profile`} width="300" height="300" />
             <div className={styles.textContainer}>
                 <p className={styles.name}>{name}</p>
--- src/pages/team.js
+++ src/pages/team.js
@@ -1,33 +1,32 @@
 import Head from "next/head";
-import { Suspense, lazy } from "react";
 import Layout from '@shared/components/Layout';
 import PageHeader from '@shared/components/PageHeader';
 import Content from "@shared/components/Content";
-import DoubleImage from "@features/team/DoubleImage";
-//import ImageFrontBig from '../../public/members/group-photos/P2026_Group_fun.jpeg';
-//import ImageBackBig from '../../public/members/group-photos/P2026_Group.jpeg';
-//import ImageFrontSmall from '../../public/members/group-photos/P2026_Group_fun.jpeg';
-//import ImageBackSmall from '../../public/members/group-photos/P2026_Group.jpeg';
 import Link from 'next/link';
+import ImageFrontBig from '../../public/members/group-photos/P2026_Group_fun.jpeg';
+import ImageBackBig from '../../public/members/group-photos/P2026_Group.jpeg';
+import ImageFrontSmall from '../../public/members/group-photos/P2026_Group_fun.jpeg';
+import ImageBackSmall from '../../public/members/group-photos/P2026_Group.jpeg';
 import styles from '@shared/styles/SubPart1.module.css';
 import { useState, useEffect } from 'react';
 import TeamOverview from "@features/team/TeamOverview";
+import DoubleImage from "@features/team/DoubleImage";
 
 export default function Team() {
     const [size, setSize] = useState('big');
-    const [imageFront, setImageFront] = useState("/members/group-photos/P2026_Group_fun.jpeg");
-    const [imageBack, setImageBack] = useState("/members/group-photos/P2026_Group.jpeg");
+    const [imageFront, setImageFront] = useState(ImageFrontBig.src);
+    const [imageBack, setImageBack] = useState(ImageBackBig.src);
 
     useEffect(() => {
         function handleResize() {
             if (window.innerWidth <= 600 && size === 'big') {
                 setSize('small');
-                setImageFront("/members/group-photos/P2026_Group_fun.jpeg");
-                setImageBack("/members/group-photos/P2026_Group.jpeg");
+                setImageFront(ImageFrontSmall.src);
+                setImageBack(ImageBackSmall.src);
             } else if (window.innerWidth > 600 && size === 'small') {
                 setSize('big');
-                setImageFront("/members/group-photos/P2026_Group_fun.jpeg");
-                setImageBack("/members/group-photos/P2026_Group.jpeg");
+                setImageFront(ImageFrontBig.src);
+                setImageBack(ImageBackBig.src);
             }
         }
         window.addEventListener('resize', handleResize)
