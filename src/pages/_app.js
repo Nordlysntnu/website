6bf7731c49264c6ad8c664caa80d10ac2d157472
Maria <145002050+marikolafs@users.noreply.github.com>
2025-10-07 20:01:24 +0200

    try to fix css imports

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -17,6 +17,10 @@ const khand = Khand({
 export default function App({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"></meta>
+            </Head>
+
             {/* Google tag (gtag.js) */}
 
             <Script
