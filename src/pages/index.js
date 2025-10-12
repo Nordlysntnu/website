f8bbf27fac4f03ad89cd76d95f4f7b2fe7418114
Maria <145002050+marikolafs@users.noreply.github.com>
2025-10-12 19:51:04 +0200

    perf: compress images and posters

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
         <SubPart1 dark={true} image="/images/compressed/LandingImage.jpg" title="The impact of creating" text={text1} link="/about" linkText="About us" />
 	<BlogPreview/>
         <Disciplines dark={true} />
-        <SubPartVideo dark={false} video="/videos/compressed/SolarVideo.mp4" poster="/posters/compressed/SolarPanels.png" title="Solar Technology" text={text2} link="/about" linkText="About us" />
+        <SubPartVideo dark={false} video="/videos/SolarVideo.mp4" poster="/posters/compressed/SolarPanels.png" title="Solar Technology" text={text2} link="/about" linkText="About us" />
       </Layout>
     </>
   )
