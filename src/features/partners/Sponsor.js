30b8028354908a6ab5d6a80e9dc1debc44af0717
Maria <145002050+marikolafs@users.noreply.github.com>
2025-10-12 16:09:42 +0200

    perf: update images to use next/image

--- src/features/partners/Sponsor.js
+++ src/features/partners/Sponsor.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import Image from 'next/image'
 import styles from './styles/Sponsor.module.css';
 
 export default function Sponsor({ name, platinum, description, link, image }) {
     return (
         <Link href={link ? link : ""} target='_blank' className={platinum ? styles.platinumContainer : styles.container}>
-            <img className={styles.image} src={image ? image : ""} />
+            <Image className={styles.image} src={image ? image : ""} />
             {platinum ?             
                 <div className={styles.textContainer}>
                     <p className={styles.description}>{description ? description : ""}</p>
