1924d2d55e5cd7a14fec69cec8404902903f7c75
Oliver Hovden <oliverhovden@protonmail.com>
2025-10-12 12:45:21 +0200

    Added CNC consult and fixed mechanical page

--- src/features/team/data/members.tsx
+++ src/features/team/data/members.tsx
@@ -165,25 +165,6 @@ export const members = [
     {
         id: 14,
 	project: [2025, 2026],
-        name: "Sander Ranbø",
-        image: "/members/2026/Sander.jpg",
-        email: "sander.ranbo@nordlysntnu.no",
-	history: [
-		{
-			year: 2025,
-		        title: "Coordinator & Mechanical Engineer Steering and Brakes",
-		        group: ["Mechanical", "All active members"]
-		},
-		{
-			year: 2026,
-				title: "Group Leader Mechanical",
-				group: ["Mechanical", "All active members"]
-		}
-	]
-    },
-    {
-        id: 15,
-	project: [2025, 2026],
         name: "Paul Gustav Aparicio Lofsberg",
         image: "/members/2026/compressed/Gustav.jpg",
 		fullImage: "/members/2026/Gustav.jpg",
@@ -202,7 +183,7 @@ export const members = [
 	]
     },
     {
-        id: 14,
+        id: 15,
 	project: [2025, 2026],
         name: "Sander Ranbø",
         image: "/members/2026/compressed/Sander.jpg",
@@ -211,7 +192,7 @@ export const members = [
 	history: [
 		{
 			year: 2025,
-		        title: "Coordinator & Mechanical Engineer Steering and Brakes",
+		        title: "Coordinator & Mechanical Engineer Suspension",
 		        group: ["Mechanical", "All active members"]
 		},
 		{
