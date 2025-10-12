9e7b27563f9293cd4fd1aaab9a57e860abb55307
Oliver Hovden <oliverhovden@protonmail.com>
2025-10-12 12:45:21 +0200

    Added CNC consult and fixed mechanical page

--- src/features/team/data/members.tsx
+++ src/features/team/data/members.tsx
@@ -165,10 +165,9 @@ export const members = [
     {
         id: 14,
 	project: [2025, 2026],
-        name: "Paul Gustav Aparicio Lofsberg",
-        image: "/members/2026/compressed/Gustav.jpg",
-		fullImage: "/members/2026/Gustav.jpg",
-        email: "paul.lofsberg@nordlysntnu.no",
+        name: "Sander Ranbø",
+        image: "/members/2026/Sander.jpg",
+        email: "sander.ranbo@nordlysntnu.no",
 	history: [
 		{
 			year: 2025,
@@ -177,27 +176,7 @@ export const members = [
 		},
 		{
 			year: 2026,
-		        title: "Group Leader Mechanical",
-		        group: ["Mechanical", "All active members"]
-		}
-	]
-    },
-    {
-        id: 15,
-	project: [2025, 2026],
-        name: "Sander Ranbø",
-        image: "/members/2026/compressed/Sander.jpg",
-		fullImage: "/members/2026/Sander.jpg",
-        email: "sander.ranbo@nordlysntnu.no",
-	history: [
-		{
-			year: 2025,
-		        title: "Coordinator & Mechanical Engineer Suspension",
-		        group: ["Mechanical", "All active members"]
-		},
-		{
-			year: 2026,
-				title: "Coordinator & Mechanical Engineer Steering and Brakes",
+				title: "Group Leader Mechanical",
 				group: ["Mechanical", "All active members"]
 		}
 	]
@@ -218,25 +197,6 @@ export const members = [
 			year: 2026,
 		        title: "Coordinator & Mechanical Engineer Suspension",
 		        group: ["Mechanical", "All active members"]
-		}
-	]
-    },
-    {
-        id: 14,
-	project: [2025, 2026],
-        name: "Sander Ranbø",
-        image: "/members/2026/Sander.jpg",
-        email: "sander.ranbo@nordlysntnu.no",
-	history: [
-		{
-			year: 2025,
-		        title: "Coordinator & Mechanical Engineer Steering and Brakes",
-		        group: ["Mechanical", "All active members"]
-		},
-		{
-			year: 2026,
-				title: "Group Leader Mechanical",
-				group: ["Mechanical", "All active members"]
 		}
 	]
     },
