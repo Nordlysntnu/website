7d52a575cb850f1089f28045d7b7782e94999cb2
Oliver Hovden <oliverhovden@protonmail.com>
2025-10-14 19:06:24 +0200

    made it sorta work. Still needs to manuall change px values to fit content and no text collapsing as of yet

--- src/features/home/BlogPreview.js
+++ src/features/home/BlogPreview.js
@@ -2,25 +2,40 @@ import styles from '@shared/styles/BlogContainer.module.css';
 import BlogEntry from '@shared/components/BlogEntry';
 import { blogs } from '@shared/data/blog';
 import Content from '@shared/components/Content'
+import Link from 'next/link';
 
 const BlogPreview = () => {
 	const BlogText = blogs[0].text;
 	const BlogImage = blogs[0].image;
 	const BlogDate = blogs[0].date;
-	return (
-	<Content blue>
-		<div style={{width: "100%", position: "absolute", zIndex:"-1"}}>
-		    <video autoPlay muted loop playsInline poster="/posters/Ribbon.png" className={styles.ribbon}>
-		        <source src="/videos/RibbonWStars.mp4" type="video/mp4" />
-		    </video>
-		</div>
-	    <div styles={{position: "relative", zIndex: "1"}}>
-			<h1 className={styles.headline}>What have we done recently?</h1>
-			<div className={styles.blogdiv}><BlogEntry text={BlogText} image={BlogImage} date={BlogDate}/></div>
-			<a href="/blog"><p className={styles.link}>See more on our blog &gt; </p></a>
-		</div>
-	</Content>
-	);
+	//return (
+	//<Content blue>
+	//	<div style={{width: "100%", position: "absolute", zIndex:"-1"}}>
+	//	    <video autoPlay muted loop playsInline poster="/posters/Ribbon.png" className={styles.ribbon}>
+	//	        <source src="/videos/RibbonWStars.mp4" type="video/mp4" />
+	//	    </video>
+	//	</div>
+	//    <div styles={{position: "relative", zIndex: "1"}}>
+	//		<h1 className={styles.headline}>What have we done recently?</h1>
+	//		<div className={styles.blogdiv}><BlogEntry text={BlogText} image={BlogImage} date={BlogDate}/></div>
+	//		<a href="/blog"><p className={styles.link}>See more on our blog &gt; </p></a>
+	//	</div>
+	//</Content>
+	//);
+    return (
+        <div className={styles.container}>
+            <div className={styles.imageContainer}>
+                <video autoPlay muted loop playsInline poster="/posters/Ribbon.png" className={styles.ribbon}>
+                    <source src="/videos/RibbonWStars.mp4" type="video/mp4" />
+                </video>
+            </div>
+            <div className={styles.textContainer}>
+				<h1 className={styles.headline}>What have we done recently?</h1>
+				<div className={styles.blogdiv}><BlogEntry text={BlogText} image={BlogImage} date={BlogDate}/></div>
+	    		<a href="/blog"><p className={styles.link}>See more on our blog &gt; </p></a>
+            </div>
+        </div>
+    )
 }
 
 export default BlogPreview;
