2b6274ba11b6ec853ccdbce8ab1fb76d6093713e
Maria <145002050+marikolafs@users.noreply.github.com>
2025-10-15 18:48:42 +0200

    small edits

--- src/shared/components/PageLanding.js
+++ src/shared/components/PageLanding.js
@@ -1,12 +1,12 @@
 import styles from '../styles/Landing.module.css';
-import Image from 'next/image'
+
 
 
 export default function PageLanding({ title, text, image }) {
     return (
         <div className={styles.container}>
             <div className={styles.imageContainer}>
-                <Image className={styles.ribbon} src={image} priority={true} />
+                <img className={styles.ribbon} src={image} priority={true} />
             </div>
             <div className={styles.textContainer}>
                 <h1 className={styles.title}>{title}</h1>
