846a811f749e559368dbb1cbe099d9178c29718d
Oliver Hovden <oliverhovden@protonmail.com>
2025-10-16 14:54:55 +0200

    started working on collapse mechanism

--- src/shared/components/BlogEntry.js
+++ src/shared/components/BlogEntry.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import styles from '../styles/BlogEntry.module.css';
 
 const BlogEntry = ({ text, image, date }) => {
+	
+	const [collapsed, setCollapsed] = useState(false);
+
 	return (
 	<div className={styles.blogEntry}>
 		<h1>{date}</h1>
-		<p>{text}</p>
+		<span>
+			<p>{text}</p>
+			<p styles={{'color': 'red'}}>Show more</p>
+		</span>
 		<img src={image} alt="failed" />
 	</div>
 	);
