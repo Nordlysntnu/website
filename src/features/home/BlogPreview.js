cea12b5b4a37a9ccf7afb90c6e6d36dc4b41eed3
Maria <145002050+marikolafs@users.noreply.github.com>
2025-10-19 14:38:47 +0200

    feat: multiple images per blog post

--- src/features/home/BlogPreview.js
+++ src/features/home/BlogPreview.js
@@ -10,7 +10,7 @@ const BlogPreview = () => {
 	<div >
 	<div className={styles.container}>
 		<h1 className={styles.headline}>Our recent activities </h1>
-		<div className={styles.blogdiv} ><BlogEntry text={BlogText} images={BlogImages} date={BlogDate}/></div>
+		<div className={styles.blogdiv} ><BlogEntry text={BlogText} image={BlogImages} date={BlogDate}/></div>
 		<a href="/blog"><p className={styles.link}>See more on our blog &gt; </p></a>
 	</div>
 	</div>
--- src/shared/data/blog.tsx
+++ src/shared/data/blog.tsx
@@ -42,7 +42,12 @@ Thank you for championing sustainable technology! ☀️ 🏎️`,
 			"/blog/unveiling.jpg",
 			"/blog/1746109403934.jpeg",
 			"/blog/1746109404280.jpeg",
-			"/blog/1746109404461.jpeg"
+			"/blog/1746109404461.jpeg",
+			"/blog/1746109404461.jpeg",
+			"/blog/1746109404461.jpeg",
+			"/blog/1746109404461.jpeg",
+			"/blog/1746109404461.jpeg",
+			"/blog/1746109404461.jpeg",
 		],
 		date: "April 2025"
 	},
@@ -55,10 +60,7 @@ Sebastian Stigar and Georgios Pantazidis provided us with valuable insights into
 - Commercial strategy 
 We found it very interesting and had the opportunity to ask many questions, receiving great answers to everything we were curious about.
 ⚡ We appreciate that they took the time to share their expertise with us. 🙏`,
-		images: [
-			"/blog/brynaarflot.jpg",
-			"/blog/bryn-bilde2.gif"
-		],
+		images: ["/blog/brynaarflot.jpg"],
 		date: "April 2025"
 	}
 ]
