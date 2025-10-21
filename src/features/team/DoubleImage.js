1096d23f730676534b23dd2cf9a6c07244ad7a20
Maria <145002050+marikolafs@users.noreply.github.com>
2025-10-21 19:21:00 +0200

    fix spelling error

--- src/features/team/DoubleImage.js
+++ src/features/team/DoubleImage.js
@@ -5,8 +5,8 @@ export default function DoubleImage({ imageFront, imageBack }) {
     return (
         <div className={styles.container}>
             <div className={styles.imageContainer}>
-                <Image src={imageFront} className={styles.image} laoding="lazy" placeholder="empty" />
-                <Image src={imageBack} className={styles.image} laoding="lazy" placeholder="empty" />
+                <Image src={imageFront} className={styles.image} loading="lazy" placeholder="empty" />
+                <Image src={imageBack} className={styles.image} loading="lazy" placeholder="empty" />
             </div>
         </div>
     )
