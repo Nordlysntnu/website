d620e386ffac32694f0068ac804ed7a5291015c3
Maria <145002050+marikolafs@users.noreply.github.com>
2025-10-26 14:24:39 +0100

    add temp icons

--- src/features/team/ContactRibbon.js
+++ src/features/team/ContactRibbon.js
@@ -1,4 +1,6 @@
-import Instagram from '@assets/symbols-and-logos/Instagram.svg';
+import Email from '@assets/symbols-and-logos/email.svg'
+import Linkedin from '@assets/symbols-and-logos/Linkedin.svg'
+import Phone from '@assets/symbols-and-logos/telephone.svg'
 import styles from './styles/ContactRibbon.module.css'
 
 export default function ContactRibbon( {email, linkedin, phone}) {
@@ -6,18 +8,18 @@ export default function ContactRibbon( {email, linkedin, phone}) {
         <div className={styles.container}>
             <div className={styles.innerContainer}>
                 <a className={styles.icon} href={`mailto:${email}`}>
-                <img src={Instagram.src} alt="Email" />
+                <img src={Email.src} alt="Email" />
                 </a>
 
                 {linkedin && (
-                    <a className={styles.icon} href={linkedin}>
-                    <img src={Instagram.src} alt="LinkedIn" />
+                    <a className={styles.icon} href={linkedin.startsWith('http') ? linkedin : `https://${linkedin}`} target="_blank" rel="noopener noreferrer" >
+                    <img src={Linkedin.src} alt="LinkedIn" />
                     </a>
                 )}
 
                 {phone && (
                     <a className={styles.icon} href={`tel:${phone}`}>
-                        <img src={Instagram.src} alt="Phone" />
+                        <img src={Phone.src} alt="Phone" />
                     </a>
                 )}
             </div>
