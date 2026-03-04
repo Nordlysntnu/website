e18f31b6350e10d5e8fb9de88d27ccb2e990804e
Oliver Hovden <oliverhovden@protonmail.com>
2026-03-04 10:03:14 +0100

    fjerna alert

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,7 @@ import Alert from '@features/alert/Alert';
 import BlogPreview from '@features/home/BlogPreview';
 import { useState } from 'react';
 
+
 export default function Home() {
   const text1 = "Theres nothing more valuable than creating something new. Through developing solar powered cars and competing in some of the worlds biggest solar racing competitions, Nordlys NTNU aims to educate the best engineers in Norway, and showcase optimism and innovation in a world in dire need of sustainable energies."
   const text2 = "One of the many contributers to solving global warming, is the increased usage of solar technology. The sun emits immense amounts of energy, free of charge. Finding innovative ways to use solar energy, such as in homes, on infrastructure, or even in cars, is key to move towards a sustainable future."
@@ -36,7 +37,6 @@ export default function Home() {
       </Head>
       <Layout current="Home" nosome={true}>
         <Landing />
-        <div style={{position: 'fixed', bottom: 16, right: 16, zIndex: 2000}}>{visiblePopup && <Alert alert={boardRecruitmentMessage} onClose = {closeAlert}/>}</div>  
         <GoalPane dark={false} />
         <SubPart1 dark={true} image="/images/compressed/LandingImage.jpg" title="The impact of creating" text={text1} link="/about" linkText="About us" />
 	      <BlogPreview/>
